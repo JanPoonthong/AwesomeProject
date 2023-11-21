4a9b8d79a3ea689c94f6cc41fc0432805ebfc940
Jan Poonthong <janpoonthong628@gmail.com>
2023-11-21 08:47:39 +0700

    learn: section and renderData of SectionList

--- App.js
+++ App.js
@@ -1,26 +1,43 @@
 import React from "react";
-import { View, Text, TextInput } from "react-native";
-import { useState } from "react";
+import { Text, FlatList, View, StyleSheet, SectionList } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 60,
+    },
+    item: {
+        padding: 10,
+        fontSize: 14,
+        height: 44,
+    },
+    sectionHeader: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 2,
+        fontSize: 14,
+        fontWeight: "bold",
+        backgroundColor: "rgba(247, 247, 247, 1.0)",
+    },
+});
 
 const App = () => {
-    const [text, setText] = useState("");
-
     return (
-        <View
-            style={{
-                marginHorizontal: 15,
-                marginVertical: 40,
-            }}
-        >
-            <TextInput
-                style={{ height: 40, borderWidth: 1, borderColor: "black" }}
-                onChangeText={newText => setText(newText)}
-                placeholder="Type here to translate!"
-                defaultValue={text}
+        <View style={styles.container}>
+            <SectionList
+                sections={[
+                    { title: "D", data: ["Devin", "Dan"] },
+                    { title: "J", data: ["Jackson", "James"] },
+                ]}
+                renderItem={({ item }) => (
+                    <Text style={styles.item}>{item}</Text>
+                )}
+                renderSectionHeader={({ section }) => (
+                    <Text style={styles.sectionHeader}>{section.title}</Text>
+                )}
+                keyExtractor={(item) => `basicListEntry-${item}`}
             />
-            <Text style={{padding: 10, fontSize: 42}}>
-                    {text.split(" ").map((word) => word && "🏝️").join(" ")}
-            </Text>
         </View>
     );
 };
